59b40c4c8df783246b4e74d43d58c6c83acd8b62
Max Hinne <mhinne@gmail.com>
2019-11-05 11:15:43 +0100

    Added all three empirical examples.

--- examples/Dutch_2017_parliament_elections.py
+++ examples/Dutch_2017_parliament_elections.py
@@ -6,9 +6,6 @@ Created on Tue Sep 10 12:30:28 2019
 """
 
 import sys
-sys.path.extend(['C:\\Users\\Max\\Dropbox\\Projects\\GPRDD'])
-sys.path.extend(['C:\\Users\\u341138\\Dropbox\\Projects\\GPRDD'])
-sys.path.extend(['C:\\Users\\Max Hinne\\Dropbox\\Projects\\GPRDD'])
 
 import GPy
 import numpy as np
@@ -18,12 +15,9 @@ import matplotlib.tri as mtri
 from matplotlib import cm
 
 import os
+# for plotting the Basemap figure, I had to add the following environment variable.
 os.environ['PROJ_LIB'] = r'C:\\Users\\u341138\\AppData\\Local\\Continuum\\anaconda3\\pkgs\\proj4-5.2.0-h6538335_1006\\Library\\share'
 
-#os.environ['PROJ_LIB'] = r'C:\\Users\\Max\\Anaconda3\\pkgs\\proj4-5.2.0-ha925a31_1\\Library\\share'
-
-#import mpl_toolkits
-#mpl_toolkits.__path__.append('C:\\Users\\Max\\Anaconda3\\envs\\gprdd36\\Lib\\site-packages\\mpl_toolkits')
 
 from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.mplot3d import Axes3D
@@ -39,11 +33,13 @@ import csv
 import warnings; warnings.simplefilter('ignore')
 
 import importlib
-import BNPQED
-importlib.reload(BNPQED)
+sys.path.append('../')
+import BNQD
+importlib.reload(BNQD)
 
 print("GPy version:      {}".format(GPy.__version__))
-print("GPRDD version:    {}".format(BNPQED.__version__))
+print("BNQD version:     {}".format(BNQD.__version__))
+
 
 plt.close('all')
 
@@ -54,8 +50,8 @@ NL_ll_lon, NL_ll_lat, NL_ur_lon, NL_ur_lat = 3.0, 50.5, 7.5, 54.0
 map_resolution = 'h'
 
           
-def read_data(votesfile='datasets\\Dutch parliament elections 2017\\Gemeente_uitslagen_TK2017.csv', 
-              geofile='datasets\\Dutch parliament elections 2017\\Gemeenten_2019_locations.csv'): 
+def read_data(votesfile='..\\datasets\\Dutch parliament elections\\Gemeente_uitslagen_TK2017.csv', 
+              geofile='..\\datasets\\Dutch parliament elections\\Gemeenten_2019_locations.csv'): 
     print('Reading data')
     
     geo_data = dict()
@@ -63,7 +59,9 @@ def read_data(votesfile='datasets\\Dutch parliament elections 2017\\Gemeente_uit
     with open(geofile, 'r') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';')
         for row in reader:
-            geo_data[row['NAAM']] = {'Provincie': row['Provincie'], 'x': float(row['Lon'].replace(',', '.')), 'y': float(row['Lat'].replace(',', '.'))}
+            geo_data[row['NAAM']] = {'Provincie': row['Provincie'], 
+                    'x': float(row['Lon'].replace(',', '.')), 
+                    'y': float(row['Lat'].replace(',', '.'))}
     
     data = dict()
     
@@ -90,7 +88,7 @@ def read_data(votesfile='datasets\\Dutch parliament elections 2017\\Gemeente_uit
                                  }
     return data
 
-def read_province_data(provincefile='datasets\\Dutch parliament elections 2017\\provinciegrenzen\\Provincies Nederand KML _ LocalFocus.xlsx'):
+def read_province_data(provincefile='..\\datasets\\Dutch parliament elections\\Provincies Nederand KML _ LocalFocus.xlsx'):
         
     def get_col_by_header(ws, value, row=1):
         for i, col in enumerate(ws.iter_cols()):
@@ -109,7 +107,8 @@ def read_province_data(provincefile='datasets\\Dutch parliament elections 2017\\
     
     for i, row in enumerate(ws_source.iter_rows(min_row=startsAt, max_row=13)):        
         province_name = row[0].value  
-        province_surface = ws_source.cell(row = i+startsAt, column = province_polygon_col).value
+        province_surface = ws_source.cell(row = i+startsAt, 
+                                          column = province_polygon_col).value
         provinces[province_name] = province_surface        
     
     province_polygons = dict()
@@ -125,12 +124,6 @@ def read_province_data(provincefile='datasets\\Dutch parliament elections 2017\\
             
     return province_polygons 
     
-
-def read_bible_belt_data(bbfile='datasets\\Dutch parliament elections 2017\\biblebelt.txt'):
-    with open(bbfile, 'r') as file:
-        biblebelt = list(set(file.read().split(', ')))
-    return biblebelt
-
 def total_vote_count(data):
     total_votes = {label:  data[label]['VVD'] + 
                            data[label]['CDA'] + 
@@ -146,18 +139,22 @@ def total_vote_count(data):
                            data[label]['FvD'] for label in data.keys()}
     return total_votes
 
-def flat_plot(x, y, z, ix, cmap=cm.viridis, marker_alpha=0.7, label1='Set 1', label2='Set 2', title=None):
+def flat_plot(x, y, z, ix, cmap=cm.viridis, marker_alpha=0.7, label1='Set 1', 
+              label2='Set 2', title=None):
     
     fig = plt.figure(figsize=(9,9))
-    m = Basemap(projection='merc', llcrnrlat=NL_ll_lat, urcrnrlat=NL_ur_lat, llcrnrlon=NL_ll_lon, urcrnrlon=NL_ur_lon, resolution=map_resolution)
+    m = Basemap(projection='merc', llcrnrlat=NL_ll_lat, urcrnrlat=NL_ur_lat, 
+                llcrnrlon=NL_ll_lon, urcrnrlon=NL_ur_lon, resolution=map_resolution)
     m.drawmapboundary(fill_color='#d8eaed')
     m.drawcountries(linewidth=1.0)
     m.drawcoastlines()
     m.fillcontinents(color='#f0efe9', lake_color='#d8eaed')
     m.scatter(x[ix], y[ix], 
-              c=z[ix], alpha=marker_alpha, cmap=cmap, marker='s', zorder=10, latlon=True, label=label1, edgecolor='k')
+              c=z[ix], alpha=marker_alpha, cmap=cmap, marker='s', zorder=10, 
+              latlon=True, label=label1, edgecolor='k')
     sc = m.scatter(x[np.logical_not(ix)], y[np.logical_not(ix)], 
-              s=30, c=z[np.logical_not(ix)], alpha=marker_alpha, cmap=cmap, marker='o', zorder=10, latlon=True, label=label2, edgecolor='k')
+              s=30, c=z[np.logical_not(ix)], alpha=marker_alpha, cmap=cmap, 
+              marker='o', zorder=10, latlon=True, label=label2, edgecolor='k')
     if title:
         plt.title(title)
     plt.legend(loc='upper left')
@@ -169,10 +166,11 @@ def flat_plot(x, y, z, ix, cmap=cm.viridis, marker_alpha=0.7, label1='Set 1', la
 
 def full_plot(x, y, z, ix, cmap=cm.summer, marker_alpha=0.7, label1='Set 1', label2='Set 2', title=None):
     
-    m = Basemap(projection='merc', llcrnrlat=NL_ll_lat, urcrnrlat=NL_ur_lat, llcrnrlon=NL_ll_lon, urcrnrlon=NL_ur_lon, resolution=map_resolution)
+    m = Basemap(projection='merc', llcrnrlat=NL_ll_lat, urcrnrlat=NL_ur_lat, 
+                llcrnrlon=NL_ll_lon, urcrnrlon=NL_ur_lon, 
+                resolution=map_resolution)
     fig = plt.figure(figsize=(9,9))
     ax = Axes3D(fig)
-#    ax = fig.gca(projection='3d')
     ax.azim = 270
     ax.elev = 90
     ax.dist = 10
@@ -189,9 +187,12 @@ def full_plot(x, y, z, ix, cmap=cm.summer, marker_alpha=0.7, label1='Set 1', lab
     
     ax.add_collection3d(lc)
     m.scatter(x[ix], y[ix], z[ix],
-              c=z[ix], alpha=marker_alpha, cmap=cmap, marker='s', zorder=10, latlon=True, label=label1, edgecolor='k')
-    m.scatter(x[np.logical_not(ix)], y[np.logical_not(ix)], z[np.logical_not(ix)],
-              s=30, c=z[np.logical_not(ix)], alpha=marker_alpha, cmap=cmap, marker='o', zorder=10, latlon=True, label=label2, edgecolor='k')
+              c=z[ix], alpha=marker_alpha, cmap=cmap, marker='s', zorder=10, 
+              latlon=True, label=label1, edgecolor='k')
+    m.scatter(x[np.logical_not(ix)], y[np.logical_not(ix)], 
+                z[np.logical_not(ix)], s=30, c=z[np.logical_not(ix)], 
+                alpha=marker_alpha, cmap=cmap, marker='o', zorder=10, 
+                latlon=True, label=label2, edgecolor='k')
     if title:
         plt.title(title)
     plt.legend(loc='best')
@@ -209,14 +210,14 @@ def flat_draw_border(ax, m, a, b, xmin=NL_ll_lon, xmax=NL_ur_lon, label='Line'):
     y = a*x + b
     
     xx, yy = m(x, y)
-    ax.plot(xx, yy, linestyle='--', linewidth=3.0, label=label, color='k', zorder=10)
+    ax.plot(xx, yy, linestyle='--', linewidth=3.0, label=label, color='k', 
+            zorder=10)
     
 def full_draw_border(fig, m, a, b, xmin=NL_ll_lon, xmax=NL_ur_lon, label='Line'):
     x = np.linspace(xmin, xmax, num=100)
     y = a*x + b
     ax = fig.gca()
     xx, yy = m(x, y)
-#    line = art3d.Line3D(xx, yy, 1e-5*np.ones((100)))
     ax.plot3D(xx, yy, 1e-5*np.ones((100)))
 
 def full_province(fig, m, province):
@@ -226,7 +227,8 @@ def full_province(fig, m, province):
         px, py = m(polygon[0], polygon[1])
         province_polys.append(np.column_stack((px, py)))
     
-    pplc = PolyCollection(province_polys, edgecolor='black', facecolor='#ffffff', closed=False)    
+    pplc = PolyCollection(province_polys, edgecolor='black', 
+                          facecolor='#ffffff', closed=False)    
     ax.add_collection3d(pplc, zs=1e-4)      
 
 def inProvince(x, province):
@@ -281,7 +283,11 @@ print('Analyze populism')
 a = 0.15
 b = 51
 border_xmin, border_xmax = (3.7, 6.75)
-y = np.array([float(data[label]['PVV'] + data[label]['SP'] + data[label]['50PLUS'] + data[label]['FvD'] ) / total_votes[label] for label in data.keys()])
+y = np.array([float(data[label]['PVV'] + \
+                    data[label]['SP'] + \
+                    data[label]['50PLUS'] + \
+                    data[label]['FvD'] ) / total_votes[label] 
+              for label in data.keys()])
 labelFunc = lambda x: [belowPhantomBorder(i, a, b) for i in x]
 
   
@@ -313,9 +319,9 @@ opts['num_restarts'] = num_restarts
 opts['mode'] = 'BIC'
 opts['verbose'] = False
 
-Total = BNPQED.BnpQedAnalysis(X, y, kernel_dict, labelFunc, b=border_pred, opts=opts)
+Total = BNQD.BnpQedAnalysis(X, y, kernel_dict, labelFunc, b=border_pred, 
+                            opts=opts)
 Total.train()
-#    total_log_bf = Total.get_total_log_Bayes_factor()
 Total.pretty_print(verbose=True)
     
 
@@ -323,17 +329,21 @@ Total.pretty_print(verbose=True)
 color_disc = '#cc7d21'
 
 # How to plot BMA here?
-fig, axes = plt.subplots(nrows=1, ncols=len(kernels), figsize=(18,4), sharex=True, sharey=True)    
+fig, axes = plt.subplots(nrows=1, ncols=len(kernels), figsize=(18,4), 
+                         sharex=True, sharey=True)    
 for i, (k, v) in zip(range(len(kernels)), kernel_dict.items()): 
 
     preds = Total.results[k].summary(b=border_pred)
     es_mean = np.array([preds['es_disc_stats'][i][0] for i in range(bn)])
     es_std  = np.array([preds['es_disc_stats'][i][1] for i in range(bn)])
                 
-    axes[i].plot(border_x1, es_mean, label='Effect size', linewidth=1.0, linestyle='--', color=color_disc)
-    axes[i].fill_between(border_x1, np.squeeze(es_mean + 0.5*es_std), np.squeeze(es_mean - 0.5*es_std), 
-        color=color_disc, alpha=0.3)
-    axes[i].annotate('log BF = {:0.1f}'.format(preds['logbayesfactor']), (3.8, 0.12))
+    axes[i].plot(border_x1, es_mean, label='Effect size', linewidth=1.0, 
+                 linestyle='--', color=color_disc)
+    axes[i].fill_between(border_x1, np.squeeze(es_mean + 0.5*es_std), 
+                         np.squeeze(es_mean - 0.5*es_std), 
+                         color=color_disc, alpha=0.3)
+    axes[i].annotate('log BF = {:0.1f}'.format(preds['logbayesfactor']), 
+                     (3.8, 0.12))
     axes[i].autoscale(enable=True, axis='x', tight=True)
     axes[i].yaxis.tick_right()
     axes[i].set_ylabel('Vote fraction')
@@ -345,7 +355,6 @@ plt.suptitle('2D effect size')
 
 total_bma = Total.get_total_log_Bayes_factor(verbose=True)
 
-# print table 4
 for kernel in kernel_names:
     summ = Total.results[kernel].summary(b=border_pred)
     print('{:s}, log BF = {:0.1f}, log p(D|M_C) = {:0.3f}, log p(D|M_D) = {:0.3f}'.format(kernel, 
@@ -353,7 +362,6 @@ for kernel in kernel_names:
           summ['evidence']['mc'],
           summ['evidence']['md']))
 
-# todo: save Total
 
 # geo map plots
 
@@ -404,7 +412,6 @@ for k in ['Linear', 'Exponential']:
     cb.set_label_text('Fraction of votes for populist parties')
     plt.suptitle('{:s} kernel, log Bayes factor = {:0.1f}'.format(k, stats['logbayesfactor']))
     print('{:s} kernel, log Bayes factor = {:0.1f}'.format(k, stats['logbayesfactor']))
-
     
     for i, X_pred_split in enumerate([X_pred[labelFunc(X_pred),:], X_pred[np.logical_not(labelFunc(X_pred)),:]]):    
         zz = np.squeeze(y_pred_d[i][0])
@@ -418,9 +425,4 @@ for k in ['Linear', 'Exponential']:
         ax.scatter(XX, YY, 
           c=y, alpha=1.0, cmap=cmap, marker='o', zorder=10, label='test', edgecolor='k')
         flat_draw_border(ax, m, a=a, b=b, label='Phantom border')
-        
-    printFig = True
-    if printFig:
-        dpi = 250
-        plt.savefig('figures\\Dutch elections\\Dutch2017_elections_{:s}_kernel_{:d}dpi_c.png'.format(k, dpi), format='png', dpi=dpi)
 
