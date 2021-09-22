Fixed bug with BMA effect size calculation; swapped PrettyTable for Pandas.

BNQD.py
@@ -2,7 +2,7 @@ import numpy as np
 import gpflow as gpf
 
 from typing import Union, Optional
-from prettytable import PrettyTable
+import pandas as pd
 from tqdm import tqdm
 from gpflow.likelihoods import Likelihood, Gaussian
 from gpflow.kernels import Kernel
@@ -10,12 +10,12 @@ from gpflow.mean_functions import MeanFunction, Constant
 from gpflow.utilities import deepcopy
 from gpflow.optimizers import Scipy
 from models import ContinuousModel, DiscontinuousModel
-from utilities import renormalize
+from utilities import renormalize, logmeanexp
 
 
 class BNQD():
 
-    __version__ = '2.0.2'
+    __version__ = '2.0.3'
 
     def __init__(self,
                  data,
@@ -109,7 +109,7 @@ class BNQD():
         assert self.is_trained, 'Model must be trained first'
 
     #
-    def train(self, opt=None, train_opts=None, verbose=False):
+    def train(self, opt=None, train_opts=None, pb=False):
         assert not self.is_trained, 'Model is already trained'
         if opt is None:
             opt = Scipy()
@@ -117,9 +117,9 @@ class BNQD():
         if train_opts is None:
             train_opts = dict()
 
-        for k in tqdm(range(len(self.kernels))):
+        for k in tqdm(range(len(self.kernels)), disable=not pb):
             for m in [self.M0[k], self.M1[k]]:
-                opt_log = opt.minimize(m.objective, m.gpmodel.trainable_variables,
+                opt.minimize(m.objective, m.gpmodel.trainable_variables,
                                        options=dict(maxiter=train_opts.get('max_iter', 10000)))
 
         self.is_trained = True
@@ -196,19 +196,20 @@ class BNQD():
     #
     def __get_effect_sizes(self):
         self.__check_training_status()
-        epsilon = 1e-6
-        K = len(self.kernels)
-        res = np.zeros((K, 2))
-        for k in range(K):
-            preds = self.M1[k].predict_y(np.atleast_2d([self.x0 - epsilon, self.x0 + epsilon]).T)
-            res[k, 0] = preds[0][1] - preds[0][0]
-            res[k, 1] = preds[1][0] + preds[1][1]
-        self.__effect_sizes = res
+        if not hasattr(self, '__effect_sizes'):
+            epsilon = 1e-6
+            K = len(self.kernels)
+            res = np.zeros((K, 2))
+            for k in range(K):
+                preds = self.M1[k].predict_y(np.atleast_2d([self.x0 - epsilon, self.x0 + epsilon]).T)
+                res[k, 0] = preds[0][1] - preds[0][0]
+                res[k, 1] = preds[1][0] + preds[1][1]
+            self.__effect_sizes = res
         return self.__effect_sizes
 
     #
     def get_effect_sizes(self):
-        return self.__effect_sizes
+        return self.__get_effect_sizes()
 
     #
     def __get_bma_effect_sizes(self):
@@ -236,13 +237,15 @@ class BNQD():
                 d_mu, d_var = es[k, :]
                 d_sd = np.sqrt(d_var)
                 self.__es_mcmc[k, 0:n_1] = np.random.normal(loc=d_mu, scale=d_sd, size=n_1)
-    #
 
+    #
     def get_bma_effect_sizes_mcmc(self):
-        return self.__es_mcmc
+        if hasattr(self, '__es_mcmc'):
+            return self.__es_mcmc
+        else:
+            return self.__get_bma_effect_sizes_mcmc()
 
     #
-
     def get_total_bma_effect_sizes_mcmc(self, nsamples=50000):
         renorm_pmp = renormalize(self.get_evidence())
         es = self.get_effect_sizes()
@@ -261,8 +264,8 @@ class BNQD():
         # zero samples from models M0
         es_total_bma.extend(np.zeros(int(np.round(nsamples * total_prob_d0))))
         return es_total_bma
-    #
 
+    #
     def get_m1_bma_effect_sizes_mcmc(self, nsamples=50000):
         renorm_pmp = renormalize(self.get_evidence()[:, 0])
         es = self.get_effect_sizes()
@@ -277,9 +280,8 @@ class BNQD():
                 d_sd = np.sqrt(d_var)
                 es_total_bma_m1.extend(np.random.normal(loc=d_mu, scale=d_sd, size=n))
         return es_total_bma_m1
+
     #
-
-
     def __get_model_posterior(self):
         self.__check_training_status()
         K = len(self.kernels)
@@ -301,32 +303,64 @@ class BNQD():
         pass
 
     #
-    def get_results(self):
-        self.__check_training_status()
-        tab = PrettyTable()
+    def conditional_bma_effectsize(self):
+        log_ml = self.__get_evidence()
+        p_k = renormalize(log_ml[:, 1])
+        es_M1 = self.__get_effect_sizes()
+        return np.dot(p_k, es_M1)
 
-        # List all kernels
-        tab.add_column('Kernel', [k.name.capitalize() for k in self.kernels])
+
+    def marginal_bma_effectsize(self, nsamples=50000):
+        renorm_pmp = renormalize(self.get_evidence())
+        es = self.get_effect_sizes()
+        total_prob_d0 = np.sum(renorm_pmp[:, 0])
+        K = len(self.kernels)
+
+        es_total_bma = []
+
+        # samples from models M1
+        for k in range(K):
+            n = int(np.round(nsamples * renorm_pmp[k, 1]))
+            if n > 0:
+                d_mu, d_var = es[k, :]
+                d_sd = np.sqrt(d_var)
+                es_total_bma.extend(np.random.normal(loc=d_mu, scale=d_sd, size=n))
+        # zero samples from models M0
+        es_total_bma.extend(np.zeros(int(np.round(nsamples * total_prob_d0))))
+        return np.asarray(es_total_bma)
+
+
+    def get_results(self):
+        # Replaced PrettyTable with pandas for easier extraction of results.
+        self.__check_training_status()
+
+        indices = [k.name.capitalize() for k in self.kernels]
+        indices += ['BMA']
+        if not self.migp:
+            data = np.zeros((len(self.kernels)+1, 8))
+        else:
+            data = np.zeros((len(self.kernels) + 1, 5))
 
         # Bayes factors
         log_bf = self.__get_bayes_factor()
-        tab.add_column('log BF', log_bf)
+        data[0:-1, 0] = log_bf
 
         # (approximated) log hyper marginal likelihood
         evidences = self.__get_evidence()
-        tab.add_column('p(D | M0)', evidences[:, 0])
-        tab.add_column('p(D | M1)', evidences[:, 1])
+        data[0:-1, 1] = evidences[:, 0]
+        data[0:-1, 2] = evidences[:, 1]
 
         # Posterior model probability
         pmp = self.__get_model_posterior()
-        tab.add_column('p(M0 | D)', pmp[:, 0])
-        tab.add_column('p(M1 | D)', pmp[:, 1])
+        data[0:-1, 3] = pmp[:, 0]
+        data[0:-1, 4] = pmp[:, 1]
 
         # Total BMA results (marginalized over kernels, assuming uniform kernel distribution)
         # BF_BMA = p(D|m1) / p(D|m0) = [\sum_k p(D|k,m1)p(k|m1)] / [\sum_k p(D|k,m0)p(k,m0)]
         # Note that we assume a uniform prior over k given m0, m1, hence we have:
         K = len(self.kernels)
-        bma_evidence = np.mean(evidences, axis=0)
+
+        bma_evidence = logmeanexp(evidences)
         bma_log_bf = bma_evidence[1] - bma_evidence[0]
         bma_pmp_m0 = 1 / (1 + np.exp(bma_log_bf))
         bma_pmp_m1 = 1 - bma_pmp_m0
@@ -334,48 +368,36 @@ class BNQD():
         if not self.migp:
             # Effect sizes given M1
             es_M1 = self.__get_effect_sizes()
-            tab.add_column('E[p(d | D, M1)]', es_M1[:, 0])
-            tab.add_column('V[p(d | D, M1)]', es_M1[:, 1])
+            data[0:-1, 5] = es_M1[:, 0]
+            data[0:-1, 6] = es_M1[:, 1]
 
             # Effect sizes given BMA (expectation only)
             es_BMA = self.__get_bma_effect_sizes()
-            tab.add_column('E[p(d | D)]', es_BMA)
+            data[0:-1, 7] = es_BMA
 
             # BMA expectation and variance (easy because this is a GMM)
-            p_D_k_m1 = renormalize(evidences[:, 1])
-            bma_m1_d_exp = np.dot(p_D_k_m1, es_M1[:, 0])
-            bma_m1_d_var = np.dot(p_D_k_m1, es_M1[:, 1])
+            bma_m1_d_exp, bma_m1_d_var = self.conditional_bma_effectsize()
 
             # total BMA expectation
             es_all = np.zeros((K, 2))
             es_all[:, 1] = es_M1[:, 0]
             p_D_k = renormalize(evidences.flatten())
             bma_es = np.dot(p_D_k, es_all.flatten())
+            # Note that this is the same as np.mean(self.marginal_bma_effectsize()), but without MC
+            data[-1, :] = [bma_log_bf, bma_evidence[0], bma_evidence[1], bma_pmp_m0, bma_pmp_m1, bma_m1_d_exp,
+                           bma_m1_d_var, bma_es]
 
-            tab.add_row(['BMA',
-                         bma_log_bf,
-                         bma_evidence[0],
-                         bma_evidence[1],
-                         bma_pmp_m0,
-                         bma_pmp_m1,
-                         bma_m1_d_exp,
-                         bma_m1_d_var,
-                         bma_es])
+        if not self.migp:
+            df = pd.DataFrame(data=data,
+                              columns=['log BF', 'p(D | M0)', 'p(D | M1)', 'p(M0 | D)', 'p(M1 | D)', 'E[p(d | D, M1)]',
+                                       'V[p(d | D, M1)]', 'E[p(d | D)]'],
+                              index=indices)
         else:
-            tab.add_row(['BMA',
-                         bma_log_bf,
-                         bma_evidence[0],
-                         bma_evidence[1],
-                         bma_pmp_m0,
-                         bma_pmp_m1])
+            df = pd.DataFrame(data=data,
+                              columns=['log BF', 'p(D | M0)', 'p(D | M1)', 'p(M0 | D)', 'p(M1 | D)'],
+                              index=indices)
 
-
-        # formatting settings
-        tab.float_format = '0.3'
-        tab.align = 'r'
-        tab.align['Kernel'] = 'l'
-
-        return tab
+        return df
 
     #
 #

test_cases/univariate/its_1d.py
@@ -19,7 +19,7 @@ from gpflow.kernels import SquaredExponential, \
 
 from gpflow.utilities import print_summary, deepcopy
 from kernels import SpectralMixture
-
+from gpflow.utilities import print_summary
 import tensorflow as tf
 import gpflow as gpf
 from utilities import plot_m0, plot_m1, plot_effect_size, split_data
@@ -43,7 +43,7 @@ def linear_regression(x, a, b, x0=0.0, d=0):
 #
 # stable: p=1, obs={g, p, t}
 
-obs_model = 't'
+obs_model = 'g'
 qed_mode = 'ITS'
 
 print('1D case; {:s} design'.format(qed_mode))
@@ -67,11 +67,11 @@ k1 = Linear()*SquaredExponential(lengthscales=1.0)
 k2 = Linear()*SquaredExponential(lengthscales=0.2)
 k = ChangePoints([k1, k2], locations=[x0], steepness=10)
 
-f = np.random.multivariate_normal(mean=np.zeros((n)), cov=k.K(x[:, None]))
+f = np.random.multivariate_normal(mean=np.zeros(n), cov=k.K(x[:, None]))
 
 
 if obs_model is 'g':
-    sigma = 0.2
+    sigma = 2.0
     y = np.random.normal(loc=f, scale=sigma)
     likelihood = Gaussian()
 elif obs_model is 'p':
@@ -96,7 +96,7 @@ print(qed.get_results())
 
 fig, axes = plt.subplots(nrows=K,
                          ncols=3,
-                         figsize=(14, K*3), sharey=True)
+                         figsize=(14, K*3), sharey='col')
 
 colors = cm.get_cmap('tab10', 10)
 
@@ -130,11 +130,11 @@ for k in range(K):
             ax=axes[k, 2],
             kernel=k,
             plot_opts=dict(color='firebrick', padding=pad))
-    axes[k, 2].plot(x[x >= x0], y[x >= x0], '.', c='k')
+    axes[k, 2].plot(x[x >= x0], y[x >= x0], 'o', c='k', fillstyle='none')
 
 
 for i, ax in enumerate(axes[:, [0, 1]].flatten()):
-    ax.plot(x, y, '.', c='k')
+    ax.plot(x, y, 'o', c='k', fillstyle='none')
     ax.axvline(x=x0, ls=':', c='k', lw=2.0)
     ax.set_xlim([xmin, xmax+pad])
 for ax in axes[:, 2]:

test_cases/univariate/rd_1d.py
@@ -1,47 +1,55 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
+import scipy.stats as st
 from BNQD import BNQD
 from gpflow.likelihoods import Gaussian, Poisson, StudentT
 from gpflow.kernels import SquaredExponential, Linear, Sum, Exponential
-
+from gpflow.utilities import print_summary
 from kernels import SpectralMixture
 
 import tensorflow as tf
 import gpflow as gpf
 from utilities import plot_m0, plot_m1, plot_effect_size
 
-plt.rc('axes', titlesize=24)        # fontsize of the axes title
-plt.rc('axes', labelsize=18)        # fontsize of the x and y labels
-plt.rc('xtick', labelsize=12)       # fontsize of the tick labels
-plt.rc('ytick', labelsize=12)       # fontsize of the tick labels
-plt.rc('legend', fontsize=20)       # legend fontsize
-plt.rc('figure', titlesize=32)      # fontsize of the figure title
+plt.rc('axes', titlesize=24)  # fontsize of the axes title
+plt.rc('axes', labelsize=18)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=12)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=12)  # fontsize of the tick labels
+plt.rc('legend', fontsize=20)  # legend fontsize
+plt.rc('figure', titlesize=32)  # fontsize of the figure title
 
 print('TensorFlow version', tf.__version__)
 print('GPflow version    ', gpf.__version__)
 print('BNQD version      ', BNQD.__version__)
 
+
 def linear_regression(x, a, b, x0=0.0, d=0):
-    return a*x + b + d*(x >= x0)
+    return a * x + b + d * (x >= x0)
+
+
+def quadratic_regression(x, b0, b1, b2, x0=0.0, d=0):
+    return b0 + b1 * x + b2 * x ** 2 + d * (x >= x0)
+
 
 obs_model = 'g'
 
 print('1D case')
-kernel_list = [Linear(), Exponential(), SquaredExponential(), SpectralMixture(Q=2)]
-# kernel_list = [Linear(), SquaredExponential()]
+# kernel_list = [Linear(), Exponential(), SquaredExponential(), SpectralMixture(Q=2)]
+kernel_list = [Linear(), SquaredExponential()]
 K = len(kernel_list)
 
 n = 50
 x0 = 0.25
-d = 3.0
+d = 0.5
 xmin, xmax = -1, 1
-# x = np.sort(np.random.uniform(low=xmin, high=xmax, size=n))
-x = np.linspace(-1, 1, num=n)
-f = linear_regression(x, a=1.3, b=3.2, x0=x0, d=d)
+x = np.sort(np.random.uniform(low=xmin, high=xmax, size=n))
+# x = np.linspace(-1, 1, num=n)
+# f = linear_regression(x, a=1.3, b=3.2, x0=x0, d=d)
+f = quadratic_regression(x, b0=1.2, b1=0.2, b2=1.7, x0=x0, d=d)
 
 if obs_model is 'g':
-    sigma = 0.4
+    sigma = 0.8
     y = np.random.normal(loc=f, scale=sigma)
     likelihood = Gaussian()
 elif obs_model is 'p':
@@ -50,7 +58,7 @@ elif obs_model is 'p':
     likelihood = Poisson()
 elif obs_model is 't':
     sigma = 1.0
-    y = f + sigma*np.random.standard_t(df=3, size=n)
+    y = f + sigma * np.random.standard_t(df=3, size=n)
     likelihood = StudentT()
 else:
     raise NotImplementedError('Unknown observation model')
@@ -63,16 +71,16 @@ qed = BNQD(data=(x, y),
            qed_mode='RD')
 qed.train()
 print('Training complete')
-print(qed.get_results())
+print(qed.get_results().to_string())
 
-fig, axes = plt.subplots(nrows=K,
+fig, axes = plt.subplots(nrows=K + 1,
                          ncols=3,
-                         figsize=(12, K*3),
+                         figsize=(12, K * 3),
                          sharex='col',
                          sharey='col')
 
-for i, ax in enumerate(axes[:, [0, 1]].flatten()):
-    ax.plot(x, y, 'o', c='k')
+for i, ax in enumerate(axes[0:-1, [0, 1]].flatten()):
+    ax.plot(x, y, 'o', c='k', fillstyle='none')
     ax.axvline(x=x0, ls=':', c='k', lw=2.0)
     ax.set_xlim([xmin, xmax])
 
@@ -107,12 +115,35 @@ for ax in axes[-1, [0, 1]]:
     ax.set_xlabel('x')
 axes[-1, -1].set_xlabel('d')
 
+
+
+d_range = np.linspace(-2, 5, num=100)
+bma_kde = st.kde.gaussian_kde(qed.marginal_bma_effectsize(nsamples=100000), bw_method='silverman')
+bma_density = bma_kde(d_range)
+axes[-1, -1].plot(d_range, bma_density, lw=2, c='g')
+axes[-1, -1].fill_between(d_range, np.zeros_like(d_range), bma_density, color='g', alpha=0.3)
+axes[-1, -1].axvline(x=0, ls='--', c='k')
+axes[-1, -1].scatter(d, 0,
+                     c='lightgrey',
+                     edgecolors='black',
+                     marker='o',
+                     s=150,
+                     zorder=10,
+                     clip_on=False,
+                     label='True discontinuity')
+
 # manually set correct limits on effect size plots...
 axes[-1, -1].set_xlim([-2, 5])
 axes[-1, -1].set_ylim([0, 0.7])
 
 axes[0, 0].set_title('Continuous')
 axes[0, 1].set_title('Discontinuous')
-plt.suptitle('{:s} observations'.format(likelihood.name.capitalize()))
+# plt.suptitle('{:s} observations'.format(likelihood.name.capitalize()))
+
+
 plt.tight_layout()
+
+plt.savefig(r'D:\SURFdrive\Teaching\Courses\2021-2022\AI for Healthcare\demo2.pdf',
+            bbox_inches='tight', pad_inches=0)
+
 plt.show()

utilities.py
@@ -42,9 +42,19 @@ def renormalize(ns):
     n = len(ns)
     threshold = np.log(epsilon) - np.log(n)
     ds = ns - mx
-    ds[ds < threshold] = 0  # throw out values below precision
+    # line below screws BMA computation and isn't needed when all models have somewhat reasonable evidence
+    # ds[ds < threshold] = 0  # throw out values below precision
     return np.exp(ds) / np.sum(np.exp(ds))
 
+
+def logsumexp(x):
+    c = x.max()
+    return c + np.log(np.sum(np.exp(x - c)))
+
+
+def logmeanexp(x):
+    c = x.max()
+    return c + np.log(np.mean(np.exp(x - c), axis=0))
 
 #
 def plot_m0(bnqd_obj, kernel, pred_range=None, ax=None, hdi=0.95, plot_opts=dict()):
@@ -65,7 +75,8 @@ def plot_m0(bnqd_obj, kernel, pred_range=None, ax=None, hdi=0.95, plot_opts=dict
     ax.plot(x_pred, mu,
             lw=plot_opts.get('linewidth', 2),
             ls=plot_opts.get('linestyle', '-'),
-            c=plot_opts.get('color', 'r'))
+            c=plot_opts.get('color', 'r'),
+            label=plot_opts.get('label', ''))
     if hdi == 0.95:
       intv = 2*np.sqrt(var)
     else:
@@ -94,13 +105,21 @@ def plot_m1(bnqd_obj, kernel, pred_range=None, ax=None, hdi=0.95, show_x0=True, 
         pred_pre = bnqd_obj.M1[kernel].predict_y(x_pred[x_pred < x0, np.newaxis])
         pred_post = bnqd_obj.M1[kernel].predict_y(x_pred[x_pred >= x0, np.newaxis])
 
-        for x, mu, var in zip([x_pred[x_pred < x0], x_pred[x_pred >= x0]],
+        for i, x, mu, var in zip([0, 1], [x_pred[x_pred < x0], x_pred[x_pred >= x0]],
                               [pred_pre[0].numpy().flatten(), pred_post[0].numpy().flatten()],
                               [pred_pre[1].numpy().flatten(), pred_post[1].numpy().flatten()]):
-            ax.plot(x, mu,
-                    lw=plot_opts.get('linewidth', 2),
-                    ls=plot_opts.get('linestyle', '-'),
-                    c=plot_opts.get('color', 'r'))
+            if i == 0:
+                ax.plot(x, mu,
+                        lw=plot_opts.get('linewidth', 2),
+                        ls=plot_opts.get('linestyle', '-'),
+                        c=plot_opts.get('color', 'r'),
+                        label=plot_opts.get('label', ''))
+            else:
+                ax.plot(x, mu,
+                        lw=plot_opts.get('linewidth', 2),
+                        ls=plot_opts.get('linestyle', '-'),
+                        c=plot_opts.get('color', 'r'))
+
             intv = var
             if hdi == 0.95:
                 intv = 2.0*np.sqrt(intv)
@@ -115,7 +134,8 @@ def plot_m1(bnqd_obj, kernel, pred_range=None, ax=None, hdi=0.95, show_x0=True, 
         ax.plot(x, mu,
                 lw=plot_opts.get('linewidth', 2),
                 ls=plot_opts.get('linestyle', '-'),
-                c=plot_opts.get('color', 'r'))
+                c=plot_opts.get('color', 'r'),
+                label=plot_opts.get('label', ''))
         intv = var
         if hdi == 0.95:
             intv = 2.0 * np.sqrt(intv)